cb58306635348c1664b418209e655ebc4b9dc1ae
Michal Matlosz <michal.matlosz@allegro.com>
2026-01-10 18:23:25 +0100

    WithMockUser

--- src/test/java/pl/umk/workshop/springintroduction/SpringSecurityTest.java
+++ src/test/java/pl/umk/workshop/springintroduction/SpringSecurityTest.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,6 +26,13 @@ class SpringSecurityTest {
                         .content("""
                                 {"student": {"name:":"Jack", "surname": "Kowalsky"},"items": ["JACKET"]}
                                 """))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturn200WhenValidAuthenticationProvided() throws Exception {
+        mockMvc.perform(delete("/deposit/1"))
                 .andExpect(status().isOk());
     }
 }
