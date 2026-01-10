c98147835ad741885ee074563cc7e61170d8a607
Michal Matlosz <michal.matlosz@allegro.com>
2026-01-10 16:12:35 +0100

    tip

--- src/test/java/pl/umk/workshop/springintroduction/SpringSecurityTest.java
+++ src/test/java/pl/umk/workshop/springintroduction/SpringSecurityTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,6 +16,9 @@ class SpringSecurityTest {
     @Autowired
     private MockMvc mockMvc;
 
+    /*
+     * TIP: sprawdź dostępne metody wywołane na anyRequest(), jedna z nich zezwala na dostęp do /deposit/ bez autoryzacji
+     */
     @Test
     void shouldReturn200WhenValidAuthenticationProvided() throws Exception {
         mockMvc.perform(delete("/deposit/1"))
