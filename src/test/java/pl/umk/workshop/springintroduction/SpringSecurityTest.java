64cb6f52fb0df141f90665a315f892905fc336f4
Michal Matlosz <michal.matlosz@allegro.com>
2026-01-10 15:59:54 +0100

    permitAll

--- src/test/java/pl/umk/workshop/springintroduction/SpringSecurityTest.java
+++ src/test/java/pl/umk/workshop/springintroduction/SpringSecurityTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,9 +19,8 @@ class SpringSecurityTest {
     private MockMvc mockMvc;
 
     @Test
-//    @WithMockUser
     void shouldReturn200WhenValidAuthenticationProvided() throws Exception {
-//        mockMvc.perform(delete("/deposit/1").with(csrf()))
-//                .andExpect(status().isOk());
+        mockMvc.perform(delete("/deposit/1"))
+                .andExpect(status().isOk());
     }
 }
