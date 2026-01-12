e2790dfe84e0c467f3773a46a309ef747bbd7948
Michal Matlosz <michal.matlosz@allegro.com>
2026-01-12 18:36:00 +0100

    test name

--- src/test/java/pl/umk/workshop/springintroduction/SpringSecurityTest.java
+++ src/test/java/pl/umk/workshop/springintroduction/SpringSecurityTest.java
@@ -20,7 +20,7 @@ class SpringSecurityTest {
      * TIP: sprawdź dostępne metody wywołane na anyRequest(), jedna z nich zezwala na dostęp do /deposit/ bez autoryzacji
      */
     @Test
-    void shouldReturn200WhenValidAuthenticationProvided() throws Exception {
+    void shouldReturn200WhenUserAuthenticated() throws Exception {
         mockMvc.perform(delete("/deposit/1"))
                 .andExpect(status().isOk());
     }
